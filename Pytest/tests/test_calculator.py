1993ffe79ac62331a677877f0ae012828ad141d5
Vontor <94050058+Vontor@users.noreply.github.com>
2025-04-01 08:12:04 +0000

    divide_test

--- Pytest/tests/test_calculator.py
+++ Pytest/tests/test_calculator.py
@@ -15,10 +15,3 @@ def test_multiply():
 
 def test_multiplywrong():
     assert calculator.multiply_wrong(6, 7) == 42
-
-def test_divide():
-    assert calculator.divide(9, 3) == 3
-
-def test_divide_by_zero():
-    with pytest.raises(ValueError, match="Cannot divide by zero!"):
-        calculator.divide(10, 0)
